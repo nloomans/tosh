48fb477726e7acdbda1be39d8e90e55776699b17
Noah Loomans <nloomans@student.codam.nl>
2019-03-21 15:32:35 +0100

    text: auto run norminette

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   strdup.c                                           :+:    :+:            */
+/*   ft_strdup.c                                        :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: nloomans <nloomans@student.codam.n>          +#+                     */
+/*   By: nloomans <nloomans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/03/20 11:28:47 by nloomans      #+#    #+#                 */
-/*   Updated: 2019/03/20 11:45:25 by nloomans      ########   odam.nl         */
+/*   Created: 2019/03/20 11:28:47 by nloomans       #+#    #+#                */
+/*   Updated: 2019/03/21 15:32:08 by nloomans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strdup(const char *s1)
 {
 	size_t	len;
 	char	*dup;
-	
+
 	len = ft_strlen(s1);
 	dup = (char *)malloc(sizeof(*dup) * len);
 	if (dup == NULL)
--- ft_strlen.c
+++ ft_strlen.c
@@ -3,16 +3,16 @@
 /*                                                        ::::::::            */
 /*   ft_strlen.c                                        :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: nloomans <nloomans@student.codam.n>          +#+                     */
+/*   By: nloomans <nloomans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/03/19 17:58:54 by nloomans      #+#    #+#                 */
-/*   Updated: 2019/03/20 11:21:14 by nloomans      ########   odam.nl         */
+/*   Created: 2019/03/19 17:58:54 by nloomans       #+#    #+#                */
+/*   Updated: 2019/03/21 15:31:52 by nloomans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-size_t ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
