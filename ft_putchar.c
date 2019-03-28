faf17e15f813db34c4d17a17bfadf9b7e690fd68
Noah Loomans <nloomans@student.codam.nl>
2019-03-28 19:23:38 +0100

    ft_putchar: remove useless import

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,11 +6,10 @@
 /*   By: nloomans <nloomans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/25 12:42:45 by nloomans       #+#    #+#                */
-/*   Updated: 2019/03/28 19:04:44 by nloomans      ########   odam.nl         */
+/*   Updated: 2019/03/28 19:11:02 by nloomans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include "libft.h"
 
 void	ft_putchar(char c)
