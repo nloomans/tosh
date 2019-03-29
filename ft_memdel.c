53936cb144cc3105352f3bc7639030f7770e1a9f
Noah Loomans <nloomans@student.codam.nl>
2019-03-29 11:17:00 +0100

    ft_{mem,str}del: don't check for NULL

--- ft_memdel.c
+++ ft_memdel.c
@@ -6,7 +6,7 @@
 /*   By: nloomans <nloomans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/25 18:29:04 by nloomans       #+#    #+#                */
-/*   Updated: 2019/03/25 18:38:11 by nloomans      ########   odam.nl         */
+/*   Updated: 2019/03/28 20:23:31 by nloomans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 void	ft_memdel(void **ap)
 {
-	if (ap == NULL)
-		return ;
 	free(*ap);
 	*ap = NULL;
 }
