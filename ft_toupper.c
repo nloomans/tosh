2d5f9b9a68752fa1d768c2ab999d53ebddbc7d26
Noah Loomans <nloomans@student.codam.nl>
2019-03-30 18:38:29 +0100

    ft_to{upper,lower}: fix

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,16 +6,13 @@
 /*   By: nloomans <nloomans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/23 19:23:45 by nloomans       #+#    #+#                */
-/*   Updated: 2019/03/25 10:43:02 by nloomans      ########   odam.nl         */
+/*   Updated: 2019/03/30 18:11:21 by nloomans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_toupper(int c)
 {
-	unsigned char	u_c;
-
-	u_c = (unsigned char)c;
-	if (u_c >= 'a' && u_c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (c + ('A' - 'a'));
 	else
 		return (c);
