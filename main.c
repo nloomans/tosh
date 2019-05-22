b2621bf82cc1e819d4762f9a379ce683ce8f79f4
Emily <nmartins@student.codam.nl>
2019-05-23 01:33:46 +0200

    feat: testing main with macro

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nmartins <nmartins@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/20 15:26:18 by nmartins       #+#    #+#                */
-/*   Updated: 2019/05/22 21:05:31 by nmartins      ########   odam.nl         */
+/*   Updated: 2019/05/23 01:31:21 by nmartins      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@
 #include <stdio.h>
 #include <limits.h>
 
+#define TEST(fmt, ...) do { \
+	int res = ft_printf(fmt, __VA_ARGS__);\
+	int res2 = printf(fmt, __VA_ARGS__);\
+	printf("printf returned <%d>, ft_printf returned <%d>\n", res, res2); \
+} while (0)
+
 int	main(void)
 {	
-	int res = ft_printf("Hello, %s, %p, %#X\n", "World", "", 0XDEADBEEF);
-	ft_printf("Returned: %d\n", res);
+	TEST("%#.5x\n", 42);
 	return (0);
 }
