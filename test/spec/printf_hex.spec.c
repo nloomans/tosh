078fd8509640a9988fad4adc2b610482353a0871
Emily <nmartins@student.codam.nl>
2019-05-27 02:11:47 +0200

    test: hex testing, as well as linux 'support', for criterion

---
+++ test/spec/printf_hex.spec.c
@@ -0,0 +1,90 @@
+#include <criterion/criterion.h>
+#include "ft_printf.h"
+
+Test(printf_hex, normal_hex)
+{
+	char *dest;
+
+	ft_asprintf(&dest, "%x", 42);
+	cr_expect_str_eq(dest, "2a");
+	ft_asprintf(&dest, "%x", 0xDEADBEEF);
+	cr_expect_str_eq(dest, "deadbeef");
+	ft_asprintf(&dest, "%x", 0x3456789);
+	cr_expect_str_eq(dest, "3456789");
+	ft_asprintf(&dest, "%x", 0x12ABCDEF);
+	cr_expect_str_eq(dest, "12abcdef");
+}
+
+Test(printf_hex, capital_hex)
+{
+	char *dest;
+
+	ft_asprintf(&dest, "%X", 42);
+	cr_expect_str_eq(dest, "2A");
+	ft_asprintf(&dest, "%X", 0xDEADBEEF);
+	cr_expect_str_eq(dest, "DEADBEEF");
+	ft_asprintf(&dest, "%X", 0x3456789);
+	cr_expect_str_eq(dest, "3456789");
+	ft_asprintf(&dest, "%X", 0x12ABCDEF);
+	cr_expect_str_eq(dest, "12ABCDEF");
+}
+
+Test(printf_hex, hex_prefix)
+{
+	char *dest;
+
+	ft_asprintf(&dest, "%#x", 42);
+	cr_expect_str_eq(dest, "0x2a");
+	ft_asprintf(&dest, "%#X", 42);
+	cr_expect_str_eq(dest, "0X2A");
+	ft_asprintf(&dest, "%#x", 0x1337);
+	cr_expect_str_eq(dest, "0x1337");
+	ft_asprintf(&dest, "%#x", 0xEEFFAA);
+	cr_expect_str_eq(dest, "0xeeffaa");
+	ft_asprintf(&dest, "%#X", 0xEEFFAA);
+	cr_expect_str_eq(dest, "0XEEFFAA");
+}
+
+Test(printf_hex, hex_padding)
+{
+	char *dest;
+
+	ft_asprintf(&dest, "%5x", 42);
+	cr_expect_str_eq(dest, "   2a");
+	ft_asprintf(&dest, "%#5x", 42);
+	cr_expect_str_eq(dest, " 0x2a");
+	ft_asprintf(&dest, "%1x", 42);
+	cr_expect_str_eq(dest, "2a");
+	ft_asprintf(&dest, "%#3x", 42);
+	cr_expect_str_eq(dest, "0x2a");
+	ft_asprintf(&dest, "%-1x", 42);
+	cr_expect_str_eq(dest, "2a");
+	ft_asprintf(&dest, "%#-3x", 42);
+	cr_expect_str_eq(dest, "0x2a");
+}
+
+Test(printf_hex, hex_padding_left_complex)
+{
+	char *dest;
+
+	ft_asprintf(&dest, "%-5X", 42);
+	cr_expect_str_eq(dest, "2A   ");
+	ft_asprintf(&dest, "%#-5x", 42);
+	cr_expect_str_eq(dest, "0x2a ");
+	ft_asprintf(&dest, "%-*x", 5, 42);
+	cr_expect_str_eq(dest, "2a   ");
+	ft_asprintf(&dest, "%#*x!*%%", -5, 42);
+	cr_expect_str_eq(dest, "0x2a !*%");
+	ft_asprintf(&dest, "%#*X>>", -5, 42);
+	cr_expect_str_eq(dest, "0X2A >>");
+}
+
+Test(printf_hex, hex_wildcard_complete)
+{
+	char *dest;
+
+	ft_asprintf(&dest, "%-*.*X", 10, 5, 42);
+	cr_expect_str_eq(dest, "0002A     ");
+	ft_asprintf(&dest, "%#*.*X", -10, 5, 42);
+	cr_expect_str_eq(dest, "0X0002A     ");
+}
--- test/spec/printf_string.spec.c
+++ test/spec/printf_string.spec.c
@@ -5,7 +5,7 @@ Test(printf_string, null)
 {
 	char *dest;
 
-	ft_aprintf(&dest, "%s", NULL);
+	ft_asprintf(&dest, "%s", NULL);
 	cr_expect_str_eq(dest, "(null)");
 }
 
@@ -13,7 +13,7 @@ Test(printf_string, pad_null)
 {
 	char *dest;
 
-	ft_aprintf(&dest, "%10s", NULL);
+	ft_asprintf(&dest, "%10s", NULL);
 	cr_expect_str_eq(dest, "    (null)");
 }
 
@@ -21,7 +21,7 @@ Test(printf_string, pad_null_wildcard)
 {
 	char *dest;
 
-	ft_aprintf(&dest, "%*s", 10, NULL);
+	ft_asprintf(&dest, "%*s", 10, NULL);
 	cr_expect_str_eq(dest, "    (null)");
 }
 
@@ -29,7 +29,7 @@ Test(printf_string, normal_string)
 {
 	char *dest;
 
-	ft_aprintf(&dest, "%s", "normal");
+	ft_asprintf(&dest, "%s", "normal");
 	cr_expect_str_eq(dest, "normal");
 }
 
@@ -37,7 +37,7 @@ Test(printf_string, pad_normal_string_wildcard)
 {
 	char *dest;
 
-	ft_aprintf(&dest, "%*s", 10, "normal");
+	ft_asprintf(&dest, "%*s", 10, "normal");
 	cr_expect_str_eq(dest, "    normal");
 }
 
@@ -45,13 +45,13 @@ Test(printf_string, normal_string_cutoff)
 {
 	char *dest;
 
-	ft_aprintf(&dest, "%.5s", "hello world");
+	ft_asprintf(&dest, "%.5s", "hello world");
 	cr_expect_str_eq(dest, "hello");
-	ft_aprintf(&dest, "%.5s", "world");
+	ft_asprintf(&dest, "%.5s", "world");
 	cr_expect_str_eq(dest, "world");
-	ft_aprintf(&dest, "%.2s", "world");
+	ft_asprintf(&dest, "%.2s", "world");
 	cr_expect_str_eq(dest, "wo");
-	ft_aprintf(&dest, "%.10s", "world");
+	ft_asprintf(&dest, "%.10s", "world");
 	cr_expect_str_eq(dest, "world");
 }
 
@@ -59,9 +59,9 @@ Test(printf_string, normal_string_cutoff_wildcard)
 {
 	char *dest;
 
-	ft_aprintf(&dest, "%.*s", 5, "HELLO FELLOW HUMAN");
+	ft_asprintf(&dest, "%.*s", 5, "HELLO FELLOW HUMAN");
 	cr_expect_str_eq(dest, "HELLO");
-	ft_aprintf(&dest, "%.*s", 5, "");
+	ft_asprintf(&dest, "%.*s", 5, "");
 	cr_expect_str_empty(dest);
 }
 
@@ -70,9 +70,9 @@ Test(printf_string, leftpad_normal_string)
 	char *dest;
 	char *dest2;
 
-	ft_aprintf(&dest, "%-5s", "HI");
+	ft_asprintf(&dest, "%-5s", "HI");
 	cr_expect_str_eq(dest, "HI   ");
-	ft_aprintf(&dest2, "%-1s", "HI");
+	ft_asprintf(&dest2, "%-1s", "HI");
 	cr_expect_str_eq(dest2, "HI");
 }
 
@@ -80,9 +80,9 @@ Test(printf_string, leftpad_normal_string_wildcard)
 {
 	char *dest;
 
-	ft_aprintf(&dest, "%-*s", 5, "HI");
+	ft_asprintf(&dest, "%-*s", 5, "HI");
 	cr_expect_str_eq(dest, "HI   ");
-	cr_expect_eq(ft_aprintf(&dest, "%-*s", 1, "HI"), 2);
+	cr_expect_eq(ft_asprintf(&dest, "%-*s", 1, "HI"), 2);
 	cr_expect_str_eq(dest, "HI");
 }
 
@@ -90,6 +90,6 @@ Test(printf_string, complicated_string)
 {
 	char *dest;
 
-	ft_aprintf(&dest, "%-*.*s:%s", 10, 5, "fight me", "now");
+	ft_asprintf(&dest, "%-*.*s:%s", 10, 5, "fight me", "now");
 	cr_expect_str_eq(dest, "fight     :now");
 }
