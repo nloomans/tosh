1277be7387249dccbf47879609366acd79da2064
Noah Loomans <noahloomans@protonmail.com>
2019-06-07 13:11:41 +0200

    fix: return -1 if ft_asprintf fails
    this was discovered while removing the unused return values

--- inc/writer.h
+++ inc/writer.h
@@ -6,7 +6,7 @@
 /*   By: nmartins <nmartins@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/20 14:59:48 by nmartins       #+#    #+#                */
-/*   Updated: 2019/06/07 12:52:53 by nloomans      ########   odam.nl         */
+/*   Updated: 2019/06/07 13:05:25 by nloomans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 typedef struct	s_writer
 {
-	ssize_t	(*write)(struct s_writer *self, char *str, size_t length);
+	void	(*write)(struct s_writer *self, char *str, size_t length);
 	void	*state;
 	size_t	written;
 	int		failed;
@@ -30,13 +30,13 @@ void			writer_write(t_writer *writer, char *str, size_t length);
 ** Write to an fd
 */
 typedef int		t_writer_fd_state;
-ssize_t			writer_fd_write(t_writer *self, char *str, size_t length);
+void			writer_fd_write(t_writer *self, char *str, size_t length);
 
 /*
 ** Write to a FILE
 */
 typedef FILE	t_writer_file_state;
-ssize_t			writer_file_write(t_writer *self, char *str, size_t length);
+void			writer_file_write(t_writer *self, char *str, size_t length);
 
 /*
 ** Write to auto-allocated string
@@ -47,13 +47,13 @@ typedef struct	s_writer_alloc_state
 	char	**str_ptr;
 	size_t	len;
 }				t_writer_alloc_state;
-ssize_t			writer_alloc_write(t_writer *self, char *str, size_t length);
+void			writer_alloc_write(t_writer *self, char *str, size_t length);
 
 typedef struct	s_writer_string_state
 {
 	char	*str_ptr;
 	ssize_t	maximum;
 }				t_writer_string_state;
-ssize_t			writer_string_write(t_writer *self, char *str, size_t length);
+void			writer_string_write(t_writer *self, char *str, size_t length);
 
 #endif
--- src/writer.c
+++ src/writer.c
@@ -6,7 +6,7 @@
 /*   By: nmartins <nmartins@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/20 15:04:21 by nmartins       #+#    #+#                */
-/*   Updated: 2019/06/07 12:18:51 by nloomans      ########   odam.nl         */
+/*   Updated: 2019/06/07 13:10:46 by nloomans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,19 @@ void	writer_write(t_writer *writer, char *str, size_t length)
 	writer->write(writer, str, length);
 }
 
-ssize_t	writer_fd_write(t_writer *self, char *str, size_t length)
+void	writer_fd_write(t_writer *self, char *str, size_t length)
 {
 	self->written += length;
-	return (write(*(t_writer_fd_state *)self->state, str, length));
+	write(*(t_writer_fd_state *)self->state, str, length);
 }
 
-ssize_t	writer_file_write(t_writer *self, char *str, size_t length)
+void	writer_file_write(t_writer *self, char *str, size_t length)
 {
 	self->written += length;
-	return (fwrite(str, length, 1, (t_writer_file_state *)self->state));
+	fwrite(str, length, 1, (t_writer_file_state *)self->state);
 }
 
-ssize_t	writer_alloc_write(t_writer *self, char *str, size_t length)
+void	writer_alloc_write(t_writer *self, char *str, size_t length)
 {
 	t_writer_alloc_state	*state;
 	char					*new;
@@ -40,17 +40,22 @@ ssize_t	writer_alloc_write(t_writer *self, char *str, size_t length)
 	state = (t_writer_alloc_state*)self->state;
 	new = ft_realloc(*state->str_ptr, state->len, state->len + length + 1);
 	if (new == NULL)
-		return (-1);
+	{
+		self->failed = 1;
+		return ;
+	}
 	*state->str_ptr = new;
 	if (ft_memcpy(*state->str_ptr + state->len, str, length) == NULL)
-		return (-1);
+	{
+		self->failed = 1;
+		return ;
+	}
 	state->len += length;
 	new[state->len] = '\0';
 	self->written += length;
-	return (length);
 }
 
-ssize_t	writer_string_write(t_writer *self, char *str, size_t length)
+void	writer_string_write(t_writer *self, char *str, size_t length)
 {
 	t_writer_string_state	*state;
 	size_t					to_write;
@@ -67,5 +72,4 @@ ssize_t	writer_string_write(t_writer *self, char *str, size_t length)
 		ft_memcpy(state->str_ptr + self->written, str, length);
 		self->written += length;
 	}
-	return (length);
 }
