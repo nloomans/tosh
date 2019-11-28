6e35d261c2c4d1d10df68b0d4f093dc259822d20
Noah Loomans <noah@noahloomans.com>
2019-11-28 15:01:17 +0100

    fix norm

--- inc/ft_getline.h
+++ inc/ft_getline.h
@@ -1,3 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_getline.h                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nloomans <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/28 14:58:41 by nloomans      #+#    #+#                 */
+/*   Updated: 2019/11/28 14:58:42 by nloomans      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GETLINE_H
+# define FT_GETLINE_H
 # define BUFF_SIZE 64
 
 typedef struct	s_fd_state_store
@@ -5,3 +19,5 @@ typedef struct	s_fd_state_store
 	int			fd;
 	char		*fd_state;
 }				t_fd_state_store;
+
+#endif
