c791303a01196a2851fbb813c65767d8425f2448
Noah Loomans <noah@noahloomans.com>
2020-02-16 17:36:27 +0100

    add unit tests for error module
    These specific unit tests are kinda useless. But it's a good example
    to show how to add unit tests.

---
+++ src/error/errorf_test.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*   tosh - 21 Shell                                      ::::::::            */
+/*                                                      :+:    :+:            */
+/*   By: ivan-tey <ivan-tey@student.codam.nl>          +:+                    */
+/*       nloomans <nloomans@student.codam.nl>         +#+                     */
+/*       aholster <aholster@student.codam.nl>        +#+                      */
+/*                                                 #+#    #+#                 */
+/*   License: GPLv3                                ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <criterion/criterion.h>
+#include "pub.h"
+
+Test(errorf, should_return_error) {
+	t_error	error;
+
+	error = errorf("unable to allocate memory");
+	cr_assert_str_eq(error.msg, "unable to allocate memory");
+}
+
+Test(errorf, should_wrap_errors) {
+	t_error	error;
+
+	error = errorf("unable to allocate memory");
+	error = errorf("failed to create object: %s", error.msg);
+	cr_assert_str_eq(error.msg,
+		"failed to create object: unable to allocate memory");
+}
+
+Test(errorf, should_handle_invalid_format_strings) {
+	t_error	error;
+
+	error = errorf("testing %q");
+	cr_assert_str_eq(error.msg,
+		"an error occurred during the generation of this error");
+}
---
+++ src/error/is_error_test.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*   tosh - 21 Shell                                      ::::::::            */
+/*                                                      :+:    :+:            */
+/*   By: ivan-tey <ivan-tey@student.codam.nl>          +:+                    */
+/*       nloomans <nloomans@student.codam.nl>         +#+                     */
+/*       aholster <aholster@student.codam.nl>        +#+                      */
+/*                                                 #+#    #+#                 */
+/*   License: GPLv3                                ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <criterion/criterion.h>
+#include "pub.h"
+
+Test(is_error, detects_errors_as_errors) {
+	cr_assert_eq(is_error(errorf("unable to allocate memory")), true);
+}
+
+Test(is_error, detects_non_errors_as_non_errors) {
+	cr_assert_eq(is_error(ERROR_NONE), false);
+}
