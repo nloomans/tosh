e41c69217f26eb9dfc64525bc72725112bffee01
Noah Loomans <noah@noahloomans.com>
2020-02-18 17:04:22 +0100

    remove useless warning when running norminette
    This is something for the sysadmin to fix, not us. By removing the
    stderr from norminette we get rid of the following output:
    
      /System/Library/Frameworks/Ruby.framework/Versions/2.3/usr/lib/ruby/2.3.0/universal-darwin18/rbconfig.rb:215: warning: Insecure world writable dir /sgoinfre in PATH, mode 040777

--- contrib/meson-norm.sh
+++ contrib/meson-norm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$0")/.." || exit 1
-! norminette "$@" src/**/*.h | tee /dev/stderr | grep -q Error
+! norminette "$@" src/**/*.h 2>/dev/null | tee /dev/stderr | grep -q Error
