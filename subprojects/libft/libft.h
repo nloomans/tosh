77adbf58c76f855e1a6080a7133a90074f2852ae
Imre van Teylingen <ivan-tey@student.codam.nl>
2020-02-22 21:05:34 +0100

    add ft_strndup
    ft_strndup is useful when you want to create a substring from another
    string

--- subprojects/libft/libft.h
+++ subprojects/libft/libft.h
@@ -48,6 +48,7 @@ void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t			ft_strlen(const char *s);
 char			*ft_strdup(const char *s1);
+char			*ft_strndup(const char *s1, size_t len);
 char			*ft_strcpy(char *dst, const char *src);
 char			*ft_strncpy(char *dst, const char *src, size_t len);
 char			*ft_strcat(char *s1, const char *s2);
