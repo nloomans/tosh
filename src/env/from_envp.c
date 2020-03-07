2909d7839499fff3399996525cd75be293ba4fbf
Imre van Teylingen <ivan-tey@student.codam.nl>
2020-03-07 11:45:00 +0100

    Split env_from_envp up in two functions, because of two many lines.

--- src/env/from_envp.c
+++ src/env/from_envp.c
@@ -13,19 +13,27 @@
 #include <assert.h>
 #include "private.h"
 
-static int		free_new(struct s_env_pair	*new)
+static int				fill_new(struct s_env_pair *new, char *envp)
 {
-	ft_strdel(&new->key);
-	ft_strdel(&new->value);
-	ft_memdel((void **)&new);
-	return (-1);
+	size_t				equal_index;
+
+	equal_index = ft_strchr(envp, '=') - envp;
+	new->key = ft_strsub(envp, 0, equal_index);
+	new->value = ft_strdup(&envp[equal_index + 1]);
+	if (new->key == NULL || new->value == NULL)
+	{
+		ft_strdel(&new->key);
+		ft_strdel(&new->value);
+		ft_memdel((void **)&new);
+		return (-1);
+	}
+	return (0);
 }
 
-int				env_from_envp(t_env *env, char **envp)
+int						env_from_envp(t_env *env, char **envp)
 {
 	size_t				i;
 	struct s_env_pair	*new;
-	size_t				equal_index;
 
 	i = 0;
 	while (envp[i] != '\0')
@@ -37,13 +45,8 @@ int				env_from_envp(t_env *env, char **envp)
 			return (-1);
 		}
 		assert(ft_strchr(envp[i], '=') != NULL);
-		equal_index = ft_strchr(envp[i], '=') - envp[i];
-		new->key = ft_strsub(envp[i], 0, equal_index);
-		new->value = ft_strdup(&envp[i][equal_index + 1]);
-		if (new->key == NULL || new->value == NULL)
-		{
-			return (free_new(new));
-		}
+		if (fill_new(new, envp[i]) == -1)
+			return (-1);
 		ft_list_insert(&env->list, env->list.last, &new->conn);
 		i++;
 	}
