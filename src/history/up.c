b628434b207e6298006b6729322d541b9a2ec090
Noah Loomans <noah@noahloomans.com>
2020-06-26 15:45:57 +0200

    history: move up and down through history

--- src/history/up.c
+++ src/history/up.c
@@ -10,43 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HISTORY_H
-# define HISTORY_H
+#include <stddef.h>
+#include "history.h"
 
-# include <libft.h>
-# include "../error/error.h"
-
-typedef struct s_history	t_history;
-
-struct						s_history_node
+char	*history_up(struct s_history *history)
 {
-	char					*buffer;
-	t_list_conn				conn;
-};
-
-struct s_history_node		*unpack_history_node(const t_list_conn *conn);
-
-struct						s_history
-{
-	int						fd;
-	t_list_meta				history;
-	t_list_conn				*cursor;
-};
-
-t_error						history_create(t_history **history,
-								const char *history_file_path);
-void						history_destroy(t_history **history);
-
-int							history_debug(void);
-
-/*
-** history_up and history_down allow you to scroll through the history.
-** Returns the buffer of the new history item, empty string if we are at the
-** bottom, and NULL of the action was a no-op.
-**
-** Result may not be  modified or freed.
-*/
-char						*history_up(struct s_history *history);
-char						*history_down(struct s_history *history);
-
-#endif
+	if (history->cursor == NULL)
+	{
+		if (history->history.last == NULL)
+			return (NULL);
+		history->cursor = history->history.last;
+	}
+	else
+	{
+		if (history->cursor->prev == NULL)
+			return (NULL);
+		history->cursor = history->cursor->prev;
+	}
+	return (unpack_history_node(history->cursor)->buffer);
+}
