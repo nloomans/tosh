nloomans/tosh

commit 92f39c04f0edef26318a5d6b48a8192833167d1f

history: address PR feedback

src/history/create.c
@@ -15,50 +15,14 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include "history.h"
+#include <limits.h>
 
-static char		*strdup_size(const char *str, size_t len)
-{
-	char	*dup;
-
-	dup = ft_strnew(len);
-	if (dup == NULL)
-		return (NULL);
-	if (str)
-		ft_strlcpy(dup, str, len + 1);
-	return (dup);
-}
-
-static t_error	read_all(char **out, int fd)
-{
-	int		ret;
-	size_t	offset;
-	size_t	size;
-
-	size = 32;
-	offset = 0;
-	while (true)
-	{
-		size *= 2;
-		ft_strreplace(out, strdup_size(*out, size));
-		if (*out == NULL)
-			return (errorf("out of memory"));
-		ret = read(fd, *out + offset, size / 2);
-		if (ret == -1)
-		{
-			ft_strdel(out);
-			return (errorf("read syscall failed"));
-		}
-		if (ret == 0)
-			return (error_none());
-		offset += ret;
-	}
-}
+#include "private.h"
 
 static t_error	fill_list(t_list_meta *list, const char *input)
 {
 	const char				*newline;
-	struct s_history_node	*new;
+	struct s_history__line	*new;
 
 	while (true)
 	{
@@ -88,13 +52,13 @@ static t_error	initialize(t_history *self, const char *history_file_path)
 	self->fd = open(history_file_path, O_RDWR | O_APPEND | O_CREAT, 0600);
 	if (self->fd == -1)
 		return (errorf("unable to open %s", history_file_path));
-	error = read_all(&contents, self->fd);
+	error = history__read_all(&contents, self->fd);
 	if (is_error(error))
 	{
 		close(self->fd);
 		return (errorf("unable to read %s: %s", history_file_path, error.msg));
 	}
-	error = fill_list(&self->history, contents);
+	error = fill_list(&self->lines, contents);
 	ft_strdel(&contents);
 	if (is_error(error))
 	{
@@ -104,7 +68,7 @@ static t_error	initialize(t_history *self, const char *history_file_path)
 	return (error_none());
 }
 
-t_error			history_create(t_history **history,
+static t_error	history_create_at(t_history **history,
 					const char *history_file_path)
 {
 	t_error	error;
@@ -119,4 +83,21 @@ t_error			history_create(t_history **history,
 		return (error);
 	}
 	return (error_none());
+}
+
+t_error			history_create(t_history **history)
+{
+	ssize_t	ret;
+	char	*home_env;
+	char	path_name[PATH_MAX];
+
+	home_env = getenv("HOME");
+	if (home_env == NULL)
+		return (errorf("HOME not set"));
+	ret = snprintf(path_name, sizeof(path_name), "%s/.tosh_history", home_env);
+	if (ret == -1)
+		return (errorf("unable to expand ~/.tosh_history"));
+	if (ret > (ssize_t)sizeof(path_name) - 1)
+		return (errorf("~/.tosh_history is longer then PATH_MAX"));
+	return (history_create_at(history, path_name));
 }

src/history/down.c
@@ -11,7 +11,8 @@
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include "history.h"
+
+#include "private.h"
 
 char	*history_down(struct s_history *history)
 {
@@ -20,5 +21,5 @@ char	*history_down(struct s_history *history)
 	history->cursor = history->cursor->next;
 	if (history->cursor == NULL)
 		return ("");
-	return (unpack_history_node(history->cursor)->buffer);
+	return (ft_strdup(unpack_line(history->cursor)->buffer));
 }

src/history/history.h
@@ -18,23 +18,7 @@
 
 typedef struct s_history	t_history;
 
-struct						s_history_node
-{
-	char					*buffer;
-	t_list_conn				conn;
-};
-
-struct s_history_node		*unpack_history_node(const t_list_conn *conn);
-
-struct						s_history
-{
-	int						fd;
-	t_list_meta				history;
-	t_list_conn				*cursor;
-};
-
-t_error						history_create(t_history **history,
-								const char *history_file_path);
+t_error						history_create(t_history **history);
 void						history_destroy(t_history **history);
 
 int							history_debug(void);
@@ -44,16 +28,15 @@ int							history_debug(void);
 ** Returns the buffer of the new history item, empty string if we are at the
 ** bottom, and NULL of the action was a no-op.
 **
-** Result may not be  modified or freed.
+** Result must be freed.
 */
-char						*history_up(struct s_history *history);
-char						*history_down(struct s_history *history);
+char						*history_up(t_history *history);
+char						*history_down(t_history *history);
 
 /*
 ** history_push adds a new item to the bottom of the history. The cursor
 ** position will be reset.
 */
-t_error						history_push(struct s_history *history,
-								const char *new);
+t_error						history_push(t_history *history, const char *new);
 
 #endif

src/bootstrap/tosh.c
@@ -21,22 +21,6 @@
 ** TODO: Consider using ft_getline if TERM is unknown.
 */
 
-static t_error	init_history(t_history **history)
-{
-	t_error	error;
-	char	*home;
-	char	*path;
-
-	home = getenv("HOME");
-	if (home == NULL)
-		return (errorf("HOME not set"));
-	if (ft_asprintf(&path, "%s/.tosh_history", home) == -1)
-		return (errorf("out of memory"));
-	error = history_create(history, path);
-	ft_strdel(&path);
-	return (error);
-}
-
 void			tosh(void)
 {
 	t_error				error;
@@ -44,11 +28,11 @@ void			tosh(void)
 	char				prompt[32];
 	t_history			*history;
 
-	error = init_history(&history);
+	error = history_create(&history);
 	if (is_error(error))
 	{
-		ft_dprintf(STDERR_FILENO, "tosh: failed to enable history suport: %s\n",
-			error.msg);
+		ft_dprintf(STDERR_FILENO,
+			"tosh: failed to enable history support: %s\n", error.msg);
 	}
 	ft_snprintf(prompt, sizeof(prompt), "%{green}TOSH $ %{reset}");
 	while (true)

src/history/debug.c
@@ -13,17 +13,17 @@
 #include <stdlib.h>
 #include <ft_printf.h>
 
-#include "history.h"
+#include "private.h"
 
 static void		print_history(const t_list_meta *history)
 {
-	const struct s_history_node	*node;
+	const struct s_history__line	*node;
 
-	node = unpack_history_node(history->first);
+	node = unpack_line(history->first);
 	while (node != NULL)
 	{
 		ft_printf("buffer |%s|\n", node->buffer);
-		node = unpack_history_node(node->conn.next);
+		node = unpack_line(node->conn.next);
 	}
 }
 
@@ -31,15 +31,11 @@ int				history_debug(void)
 {
 	t_error		error;
 	t_history	*history;
-	char		*path;
 
-	if (ft_asprintf(&path, "%s/.tosh_history", getenv("HOME")) == -1)
-		return (ft_eprintf(1, "tosh: out of memory\n"));
-	error = history_create(&history, path);
-	ft_strdel(&path);
+	error = history_create(&history);
 	if (is_error(error))
 		return (ft_eprintf(1, "tosh: %s\n", error.msg));
-	print_history(&history->history);
+	print_history(&history->lines);
 	history_destroy(&history);
 	return (0);
 }

src/history/meson.build
@@ -4,6 +4,7 @@ src_files += files(
 	'destroy.c',
 	'down.c',
 	'push.c',
+	'read_all.c',
 	'unpack.c',
 	'up.c',
 )

src/history/private.h
@@ -10,21 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#ifndef PRIVATE_H
+# define PRIVATE_H
 
-#include "private.h"
+# include "history.h"
 
-void			history_destroy(t_history **history)
+struct						s_history__line
 {
-	struct s_history__line	*line;
+	char					*buffer;
+	t_list_conn				conn;
+};
 
-	close((*history)->fd);
-	while ((*history)->lines.len > 0)
-	{
-		line = unpack_line((*history)->lines.first);
-		ft_list_unlink(&(*history)->lines, (*history)->lines.first);
-		ft_strdel(&line->buffer);
-		ft_memdel((void **)&line);
-	}
-	ft_memdel((void **)history);
-}
+struct s_history__line		*unpack_line(const t_list_conn *conn);
+
+struct						s_history
+{
+	int						fd;
+	t_list_meta				lines;
+	t_list_conn				*cursor;
+};
+
+t_error						history__read_all(char **out, int fd);
+
+#endif

src/history/push.c
@@ -13,23 +13,23 @@
 #include <libft.h>
 #include <ft_printf.h>
 
-#include "history.h"
+#include "private.h"
 
 t_error						history_push(struct s_history *history,
 								const char *new_buffer)
 {
-	struct s_history_node *node;
+	struct s_history__line	*line;
 
-	node = ft_memalloc(sizeof(*node));
-	if (node == NULL)
+	line = ft_memalloc(sizeof(*line));
+	if (line == NULL)
 		return (errorf("out of memory"));
-	node->buffer = ft_strdup(new_buffer);
-	if (node->buffer == NULL)
+	line->buffer = ft_strdup(new_buffer);
+	if (line->buffer == NULL)
 	{
-		ft_memdel((void **)&node);
+		ft_memdel((void **)&line);
 		return (errorf("out of memory"));
 	}
-	ft_list_insert(&history->history, history->history.last, &node->conn);
+	ft_list_insert(&history->lines, history->lines.last, &line->conn);
 	history->cursor = NULL;
 	if (ft_dprintf(history->fd, "%s\n", new_buffer) == -1)
 		return (errorf("failed to write to history file"));

commit c1efda729af245db34ecd260a714e7ed81eae0fe

input: fix "pointer being freed was not allocated" abort
A bug existed where the program would crash when moving past the most
recent history item.

Steps to reproduce:

 1. Have at least one item in ~/.tosh_history
 2. Launch ./tosh
 3. Press up
 4. Press down
 5. Press up

Result:

    $ ./tosh
    TOSH $ tosh(15515,0x10c95e5c0) malloc: *** error for object 0x1018f9690: pointer being freed was not allocated
    tosh(15515,0x10c95e5c0) malloc: *** set a breakpoint in malloc_error_break to debug
    [1]    15515 abort      ./tosh

This was caused by a regression in 92f39c0 ("input: address PR
feedback") where the history_down function was altered to return a
allocated string. Unfortunately one case was missed.

A test has also been added to catch future bugs like this.

src/history/create.c
@@ -68,7 +68,7 @@ static t_error	initialize(t_history *self, const char *history_file_path)
 	return (error_none());
 }
 
-static t_error	history_create_at(t_history **history,
+t_error			history_create_at(t_history **history,
 					const char *history_file_path)
 {
 	t_error	error;

src/history/down.c
@@ -20,6 +20,6 @@ char	*history_down(struct s_history *history)
 		return (NULL);
 	history->cursor = history->cursor->next;
 	if (history->cursor == NULL)
-		return ("");
+		return (ft_strdup(""));
 	return (ft_strdup(unpack_line(history->cursor)->buffer));
 }

src/history/history.h
@@ -19,6 +19,8 @@
 typedef struct s_history	t_history;
 
 t_error						history_create(t_history **history);
+t_error						history_create_at(t_history **history,
+								const char *history_file_path);
 void						history_destroy(t_history **history);
 
 int							history_debug(void);

src/input/run_next_action_test.c
@@ -10,9 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#define _POSIX_C_SOURCE 200809L
 #include <criterion/criterion.h>
 #include <unistd.h>
 #include <libft.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 #include "private.h"
 
@@ -138,4 +141,41 @@ Test(input__run_next_action, read_fails) {
 	cr_expect_str_eq(error.msg, "failed to read keypress");
 
 	free(state.buffer);
+}
+
+size_t index_history = 0;
+
+char history_message[] = "\x1b[A\x1b[B\x1b[A";
+
+ssize_t fake_read_history(int fd, void *buf, size_t count) {
+	cr_expect_eq(fd, STDIN_FILENO);
+	strncpy(buf, history_message + index_history, count);
+	index_history += count;
+	return (count);
+}
+
+Test(input__run_next_action, history) {
+	struct s_input__state state = {
+		.buffer = strdup(""),
+	};
+
+	int fd = open("/tmp/tosh_history", O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	dprintf(fd, "ls -al\n");
+	close(fd);
+	history_create_at(&state.history, "/tmp/tosh_history");
+
+	while (index_history < strlen(history_message))
+	{
+		bool did_invalidate = false;
+		t_error error = input__run_next_action(
+			&state, NULL, &did_invalidate, fake_read_history);
+		cr_expect(did_invalidate);
+		cr_expect_not(is_error(error));
+	}
+
+	cr_expect_str_eq(state.buffer, "ls -al");
+	cr_expect_eq(state.cursor_position, 6);
+
+	free(state.buffer);
+	history_destroy(&state.history);
 }