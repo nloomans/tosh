d944aaebe0589ebff4de19f0cd4d86122c6b2884
Noah Loomans <noah@noahloomans.com>
2020-07-14 11:27:31 +0200

    parser: error if not all tokens are parsed
    This otherwise breaks the assumptions that exec makes which is that the
    AST will contain items if we have tokens.
    
        TOSH $ |
        fish: “./tosh” terminated by signal SIGSEGV (Address boundary error)

--- src/parser/complete_command.c
+++ src/parser/complete_command.c
@@ -10,8 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <assert.h>
 #include <stdlib.h>
 #include "private.h"
+
+static void					check_unexpected_token(t_parser *const p)
+{
+	struct s_token *unexpected;
+
+	unexpected = parser__peek_token(p);
+	if (!unexpected)
+		return ;
+	if (unexpected->type == WORD)
+		parser__errorf(p, "unexpected word '%s'", unexpected->string);
+	else if (unexpected->type == OPERATOR)
+		parser__errorf(p, "unexpected operator '%s'", unexpected->string);
+	else if (unexpected->type == IO_NUMBER)
+		parser__errorf(p, "unexpected io_number '%s'", unexpected->string);
+	else
+		assert(0);
+}
 
 struct s_complete_command	*parse_complete_command(t_parser *const p)
 {
@@ -19,12 +37,12 @@ struct s_complete_command	*parse_complete_command(t_parser *const p)
 
 	complete_command = ft_memalloc(sizeof(*complete_command));
 	complete_command->list = parse_list(p);
+	check_unexpected_token(p);
 	if (!complete_command->list)
 	{
 		free_complete_command(complete_command);
 		return (NULL);
 	}
-	parser__next_if_token(p, OPERATOR, ";");
 	return (complete_command);
 }
 
