17deb5261b97e8d133f5d0e5aa2ea727140f3b0a
aholster <aholster@student.codam.nl>
2020-07-21 16:48:08 +0200

    fixed cd

--- src/exec/builtin/cd.c
+++ src/exec/builtin/cd.c
@@ -20,13 +20,14 @@
 
 static uint8_t	cd(t_env *const env,
 				const char *const old_path,
+				const bool is_old_set,
 				const char *const new_path)
 {
 	if (chdir(new_path) == -1)
 	{
 		return (ft_eprintf(1, "cd: unable to change dir '%s'\n", new_path));
 	}
-	if (env_set(env, "OLDPWD", old_path) == -1)
+	if (is_old_set == true && env_set(env, "OLDPWD", old_path) == -1)
 	{
 		return (ft_eprintf(2, "cd: could not set $OLDPWD\n"));
 	}
@@ -41,9 +42,11 @@ uint8_t			builtin_cd(int argc, char **argv, t_env *const env)
 {
 	char	old_path[PATH_MAX];
 	char	new_path[PATH_MAX];
+	bool	is_old_set;
 
+	is_old_set = true;
 	if (getcwd(old_path, PATH_MAX) == NULL)
-		return (ft_eprintf(1, "cd: getcwd syscall failed\n"));
+		is_old_set = false;
 	if (argc > 2)
 		return (ft_eprintf(1, "cd: to many arguments\n"));
 	if (argc == 1)
@@ -61,5 +64,5 @@ uint8_t			builtin_cd(int argc, char **argv, t_env *const env)
 	}
 	else
 		ft_strlcpy(new_path, argv[1], PATH_MAX);
-	return (cd(env, old_path, new_path));
+	return (cd(env, old_path, is_old_set, new_path));
 }
