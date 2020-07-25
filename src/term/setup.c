2e9e75a852dedf5c20cfc860730667f393bfc02e
Noah Loomans <noah@noahloomans.com>
2020-07-25 20:29:00 +0200

    term: fix norm

--- src/term/setup.c
+++ src/term/setup.c
@@ -50,7 +50,7 @@ t_error			term_setup(t_term **out, const char *term_env)
 	if (tgetent(NULL, term_env) != 1)
 	{
 		if (tgetent(NULL, "xterm") != 1)
-			return (errorf("tgetent failed: unknown $TERM and xterm is invalid"));
+			return (errorf("tgetent failed: unknown $TERM, xterm is invalid"));
 	}
 	error = configure(&term);
 	if (is_error(error))
