71dfdd42d8085102b23b9c7c94a4d612d2c8c37b
aholster <aholster@student.codam.nl>
2020-08-17 13:18:41 +0200

    fixed lack of malloc check

--- src/exec/quote_and_expansion/replacer_fsm.c
+++ src/exec/quote_and_expansion/replacer_fsm.c
@@ -122,6 +122,8 @@ t_error			replacer_fsm(char **const tape,
 		((*tape)[1] == '\0' || (*tape)[1] == '/'))
 	{
 		ft_strreplace(&new_tape, env_get(env, "HOME"));
+		if (new_tape == NULL)
+			return (errorf("unable to allocate memory"));
 		err = iter_fsm((*tape) + 1, &new_tape, machine, env);
 	}
 	else
